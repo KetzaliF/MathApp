4a01a9b641c70186e637dac840594f439e04dc41
KetzaliF <ketzalihoffman@gmail.com>
2022-03-05 12:56:58 -0600

    Theme change fix

--- scripts/theme.js
+++ scripts/theme.js
@@ -1,15 +1,22 @@
-const icon = document.querySelector('.icon-theme');
+const iconSun = document.querySelector('.theme__sun');
+const iconMoon = document.querySelector('.theme__moon');
 
 document.querySelector(".theme").addEventListener("click", () => {
    if (document.body.classList.contains('dark')) {
       document.body.classList.remove('dark');
       document.body.classList.add('light');
-      icon.src = '../assets/dark-mood.png';
+      
+      iconMoon.style.display = 'initial';
+      iconSun.style.display = 'none';
+
       localStorage.setItem("theme", "light");
    } else {
       document.body.classList.remove('light');
       document.body.classList.add('dark');
-      icon.src = '../assets/light-mood.png';
+
+      iconMoon.style.display = 'none';
+      iconSun.style.display = 'initial';
+
       localStorage.setItem("theme", "dark");
    }
 })
@@ -18,11 +25,15 @@ const saveTheme = () =>{
    if(localStorage.getItem('theme') === 'dark'){
       document.body.classList.remove('light');
       document.body.classList.add('dark');
-      icon.src = '../assets/light-mood.png';
+
+      iconMoon.style.display = 'none';
+      iconSun.style.display = 'initial';
    }else{
       document.body.classList.remove('dark');
       document.body.classList.add('light');
-      icon.src = '../assets/dark-mood.png';
+
+      iconMoon.style.display = 'initial';
+      iconSun.style.display = 'none';
    }
 }
 
